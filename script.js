e4b059ee2890973b33765f6edc70c9ae7266d273
Всеволод <kreepmeister@gmail.com>
2024-05-05 12:44:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Jun 1, 2024 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="timer"
+    //document.getElementById("timer").innerHTML = days + "дни " + hours + "часы "
+    //    + minutes + "мин " + seconds + "сек ";
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("timer").innerHTML = "ПРОШЛА";
+    }
+}, 1000);
